990aa9aa8d46c0060ce298756d8db57917768867
srgrantuw <104274014+srgrantuw@users.noreply.github.com>
2022-04-23 14:50:31 -0400

    Initial commit

---
+++ Source/Underworld/UnderworldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnderworldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNDERWORLD_API AUnderworldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
